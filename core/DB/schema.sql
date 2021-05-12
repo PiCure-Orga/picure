4d4ac4aa74826985dc8dda6d9317f659026e9a5d
mhupfauer <markus.hupfauer11@gmail.com>
2021-05-12 22:56:02 +0200

    Added table for long term storage of sensor values
    Signed-off-by: mhupfauer <markus.hupfauer11@gmail.com>

--- core/DB/schema.sql
+++ core/DB/schema.sql
@@ -6,3 +6,9 @@ CREATE TABLE latest_10_minutes(
     sensor TEXT,
     value DECIMAL(4,2)
 )
+CREATE TABLE latest_30_minutes(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    timestamp INTEGER,
+    sensor TEXT,
+    value DECIMAL(4,2)
+)
