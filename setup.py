6a1dd5f868638648aae7ce30b4be4abb7638c45b
Markus Hupfauer <markus.hupfauer@xitaso.com>
2021-05-12 16:56:37 +0200

    Installer fixed

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.0.1',
     packages=find_packages(),
     include_package_data=True,
+    zip_safe=False,
     url='https://github.com/mhupfauer/picure',
     license='GNU General Public License v3.0  ',
     author='Markus Hupfauer',
