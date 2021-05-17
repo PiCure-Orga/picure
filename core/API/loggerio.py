94852cd7cd28fc0e7d672a7fc8968e9160f31a47
mhupfauer <markus.hupfauer11@gmail.com>
2021-05-18 00:57:57 +0200

    API endpoint /data/<sensor>/minutes now supports a list of sensors separated by comma
    Signed-off-by: mhupfauer <markus.hupfauer11@gmail.com>

--- core/API/loggerio.py
+++ core/API/loggerio.py
@@ -1,39 +1,34 @@
-from flask import Blueprint, abort, request
+from flask import Blueprint, abort, request, escape
 from core.DB import db_handler
 import json
 
 loggerio = Blueprint("loggerio", __name__, template_folder="templates")
 
 
-@loggerio.route("/data/<minutes>/<sensor>", methods=["GET"])
+@loggerio.route("/data/<sensor>/<minutes>", methods=["GET"])
 def get_sensor_data(minutes, sensor):
     db = db_handler.get_db()
-    result = None
 
+    sensor = escape(sensor)
+    minutes = escape(minutes)
+
+    sensors = " ".join('"{}",'.format(i) for i in sensor.split(","))
     minutes = int(minutes)
 
     if minutes > 10:
-        result = (
-            db.cursor()
-            .execute(
-                "SELECT timestamp, sensor, value from latest_30_days where sensor in (?) order by id desc limit ?",
-                (sensor, minutes),
-            )
-            .fetchall()
-        )
-
+        db_name = "latest_30_days"
     elif 0 < minutes <= 10:
-        result = (
-            db.cursor()
-            .execute(
-                "SELECT timestamp, sensor, value from latest_10_minutes where sensor in (?) order by id desc limit ?",
-                (sensor, minutes * 6),
-            )
-            .fetchall()
-        )
-
+        db_name = "latest_10_minutes"
     else:
         abort(500, "Valid minutes must not exceed integer range or be negative")
+
+    # I fully understand that this is not a good idea. However passing the comma separated list of sensors
+    # escapes the required quotes and commas. There is escaping done above so this should be fine...
+    cmd = "SELECT timestamp, sensor, value from {} where sensor in ({}) order by id desc limit {}".format(
+        db_name, sensors[:-1], minutes
+    )
+
+    result = db.cursor().execute(cmd).fetchall()
 
     res = [(d["timestamp"], d["sensor"], d["value"]) for d in result]
     return json.dumps(res)
