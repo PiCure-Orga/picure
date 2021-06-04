06b6d55cb25b36757915658cf0fc6233dc58664a
mhupfauer <markus.hupfauer11@gmail.com>
2021-06-04 17:30:38 +0200

    Make flake8 happy
    Signed-off-by: mhupfauer <markus.hupfauer11@gmail.com>

--- picure/API/run.py
+++ picure/API/run.py
@@ -27,7 +27,6 @@
 #      You should have received a copy of the GNU General Public License
 #      along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import json
-import time
 
 from flask import Blueprint, request, abort, Response
 from picure.Backend.DB.db_handler import get_db
@@ -40,7 +39,9 @@ def get_run():
     runs = (
         get_db()
         .cursor()
-        .execute("SELECT pr.id as id, pr.program_id as program_id, pr.enabled as enabled, pr.start_timestamp as start_timestamp, p.name as name FROM program_run pr join program p on pr.program_id = p.id")
+        .execute(
+            "SELECT pr.id as id, pr.program_id as program_id, pr.enabled as enabled, pr.start_timestamp as start_timestamp, p.name as name FROM program_run pr join program p on pr.program_id = p.id"
+        )
         .fetchall()
     )
     return json.dumps(
@@ -56,28 +57,33 @@ def get_run():
         ]
     )
 
-@run.route("/api/run/<int:run_id>", methods=['DELETE'])
+
+@run.route("/api/run/<int:run_id>", methods=["DELETE"])
 def delete_run(run_id):
     get_db().cursor().execute("DELETE FROM program_run where id=?", (run_id,))
     get_db().commit()
     get_db().cursor().close()
     return Response(response="Deleted program run", status=204)
 
-@run.route("/api/run", methods=['POST'])
+
+@run.route("/api/run", methods=["POST"])
 def add_run():
     form = request.form.to_dict()
     if not ("program_id" in form and "enabled" in form):
         abort(500)
 
-    enabled = (1 if form["enabled"].lower() == "true" else 0)
+    enabled = 1 if form["enabled"].lower() == "true" else 0
 
     get_db().cursor().execute("UPDATE program_run SET enabled = 0")
-    get_db().cursor().execute("INSERT INTO program_run (program_id, enabled, start_timestamp ) values (?,?, (SELECT strftime('%s','now')) )",
-                              (form["program_id"], enabled))
+    get_db().cursor().execute(
+        "INSERT INTO program_run (program_id, enabled, start_timestamp ) values (?,?, (SELECT strftime('%s','now')) )",
+        (form["program_id"], enabled),
+    )
     get_db().commit()
     get_db().cursor().close()
 
     return Response(response="Program run started", status=204)
+
 
 @run.route("/api/run/<int:run_id>", methods=["PATCH"])
 def patch_run(run_id):
@@ -85,7 +91,7 @@ def patch_run(run_id):
     if not ("enabled" in form and len(form) == 1):
         abort(500)
 
-    enabled = (1 if form["enabled"].lower() == "true" else 0)
+    enabled = 1 if form["enabled"].lower() == "true" else 0
 
     get_db().cursor().execute(
         "UPDATE program_run SET enabled=:enabled where id=:run_id",
